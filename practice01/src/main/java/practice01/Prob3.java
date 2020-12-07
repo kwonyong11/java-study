e6a0c29e2e87893800dc4d5b191d4763c5e8cb07
BIT_R <BIT_R@DESKTOP-CTRFTLR>
2020-12-07 18:58:11 +0900

    practice01 Prob5까지

---
+++ practice01/src/main/java/practice01/Prob3.java
@@ -0,0 +1,26 @@
+package practice01;
+
+import java.util.Scanner;
+
+public class Prob3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.print("숫자를 입력하세요: ");
+		int num=scan.nextInt();
+		int result=0;
+		int check=0;
+		if(num%2==0) {
+			check=2;
+		}
+		else {
+			check=1;
+		}
+		for(int i=check;i<=num;i+=2) {
+			result+=i;
+		}
+		System.out.println("결과 값 : "+result);
+	}
+
+}
---
+++ practice01/src/main/java/practice01/Prob4.java
@@ -0,0 +1,20 @@
+package practice01;
+
+import java.util.Scanner;
+
+public class Prob4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.print("문자열을 입력하세요. : ");
+		String str=scan.nextLine();
+		String result="";
+		for(int c=0;c<str.length();c++) {
+			result+=str.charAt(c);
+			System.out.println(result);
+		}
+	}
+
+}

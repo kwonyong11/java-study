7d8226ee651d921f2962691a096e924407a0a37f
kwonyong11 <rnjsdyd456@naver.com>
2020-12-14 16:05:49 +0900

    TCP 소켓 프로그래밍 연습#3

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,8 +1,12 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TCPServer {
 
@@ -18,14 +22,56 @@ public class TCPServer {
 			serverSocket.bind(inetSocketAddress);
 			
 			//3. accept
-			serverSocket.accept(); // blocking
+			Socket socket=serverSocket.accept(); // blocking
 			
-			System.out.println("dfsdfsdfsdf");
+			InetSocketAddress remoteInetSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+			
+			InetAddress remoteInetAddress = remoteInetSocketAddress.getAddress();
+			String remoteHostAddress = remoteInetAddress.getHostAddress();
+			int remotePort = remoteInetSocketAddress.getPort();
+			
+			System.out.println("[server] connected by client["+remoteHostAddress+":"+remotePort+"]");
+			
+			try {
+				//4. IOStream 받아 오기
+				InputStream is =socket.getInputStream();
+				OutputStream os=socket.getOutputStream();
+				
+				while(true) {
+					//5. 데이터 읽기
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer); // blocking
+					
+					if(readByteCount == -1) {
+						//client가 소켓을 정상적으로 종료
+						System.out.println("[server] closed by client");
+						break;
+					}
+					
+					String data=new String(buffer, 0, readByteCount,"UTF-8");
+					System.out.println("[server] received:"+data);
+					
+					//6. 데이터쓰기
+					os.write(data.getBytes("UTF-8"));
+				}
+			}catch(IOException e) {
+				System.out.println("[server] error:"+e);
+			}finally {
+				try {
+					if(socket != null && !socket.isClosed()) {
+						socket.close();
+					}
+				}
+				catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
 		} catch (IOException e) {
 			System.out.println("[server] error:" + e);
 		}finally {
 			try {
-				if(serverSocket != null && serverSocket.isClosed()) {
+				if(serverSocket != null && !serverSocket.isClosed()) {
 					serverSocket.close();
 				}
 			} catch (IOException e) {

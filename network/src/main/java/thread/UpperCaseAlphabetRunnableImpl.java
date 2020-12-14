35b28edff070d755ab9bd5a30c9c7caec734586c
kwonyong11 <rnjsdyd456@naver.com>
2020-12-14 16:35:48 +0900

    쓰레드 프로그래밍 기초

---
+++ network/src/main/java/thread/UpperCaseAlphabetRunnableImpl.java
@@ -0,0 +1,11 @@
+package thread;
+
+public class UpperCaseAlphabetRunnableImpl extends UpperCastAlphabet implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		print();
+	}
+	
+}
